7de24ecdcaa7ab7804715148667a7155b2247c0a
kelicia91 <kelicia91@gmail.com>
2021-10-09 23:47:57 +0900

    test: car.move()

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -2,23 +2,26 @@ package racinggame;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import nextstep.test.NSTest;
 
 class CarTest extends NSTest {
 
 	private static final int MOVING_DISTANCE_MIN = 0;
-	private static final int CAN_MOVE_FORWARD = 4;
-	private static final int HOW_MANY_DISTANCE_MOVE_FORWARD = 1;
 
-	@Test
-	void move() {
+	@ParameterizedTest
+	@CsvSource(value = {
+		"4:1", // CAN_MOVE:HOW_MANY_DISTANCE_MOVE
+		"0:0"  // NOT_MOVE:MOVING_DISTANCE_MIN
+	}, delimiter = ':')
+	void move(int random, int movingDistance) {
 		Car car = new Car("name");
 		assertThat(car.getMovingDistance()).isEqualTo(MOVING_DISTANCE_MIN);
 		assertRandomTest(() -> {
-			assertThat(car.move()).isEqualTo(HOW_MANY_DISTANCE_MOVE_FORWARD);
-		}, CAN_MOVE_FORWARD);
+			assertThat(car.move()).isEqualTo(movingDistance);
+		}, random);
 	}
 
 	@Override
